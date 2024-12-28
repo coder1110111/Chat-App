8bb1bd38cea6075def11ce849c98aeee3cef0608
coder1110111 <ayush.yadav.dev610>
2024-12-28 06:21:03 +0530

    Final basic app that works

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app=express();
 app.use(bodyparse.urlencoded({extended:false}));
 
 app.get('/login',(req,res,next)=>{                                  //can be moved to login.js
-    console.log("This is login page.");
+    //console.log("This is login page.");
     res.send(`<html><body><form action="/login" method="POST"><input type="text" name="username"><button type="submit">Login</button>`)
 })
 app.post('/login',(req,res)=>{              //can be moved to login.js
@@ -29,12 +29,31 @@ app.post('/login',(req,res)=>{              //can be moved to login.js
 
 app.get('/',(req,res)=>{
     fs.readFile("textchat.txt",(err,data)=>{
-        console.log(data);
-        res.send(`<form action="/" method="POST"><p>${data.toString()}<p><input type="text" name="message"><button type="submit">Send</button></form>`)
+        //console.log(data.toString());
+        res.send(`<form action="/" method="POST">
+                    <p>${data.toString()}<p>
+                    <input type="text" name="message">
+                    <input type="hidden" id="username" name="username">
+                    <button type="submit">Send</button>
+                  </form>
+                  <script>
+                    window.onload = function(){
+                        const username=localStorage.getItem('username');
+                        if(!username){
+                            alert("You are not logged in!");
+                            window.location.href='/login';
+                        } else{
+                            console.log(username);
+                            document.getElementById("username").value=username;
+                        }
+                    };
+                  </script>
+        `)
     })
 })
 app.post('/',(req,res)=>{
     let username=req.body.username;
+    console.log(username);
     let message=req.body.message;
     let formattedtext=` ${username}: ${message}`;
     fs.appendFile("textchat.txt",formattedtext,(err)=>{
